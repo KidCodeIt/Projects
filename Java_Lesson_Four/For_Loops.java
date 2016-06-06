7b2f02056a40fca59e73af1f297d25a02c32147c
Scott Campana <sbcampana@gmail.com>
2016-06-06 12:52:17 -0600

    Add files via upload

---
+++ Java_Lesson_Four/For_Loops.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class For_Loops here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class For_Loops
+{
+    public static void main(String [] args){
+        System.out.println("This counts from 1 to 10");
+        
+        //for loop that prints out the number of times it has gone through
+        //1 to 10
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        
+    }
+}
