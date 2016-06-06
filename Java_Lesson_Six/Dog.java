1f38caca0dfbea777b393abe115daae910c2ab9d
Scott Campana <sbcampana@gmail.com>
2016-06-06 12:53:32 -0600

    Add files via upload

---
+++ Java_Lesson_Six/Dog.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Dog here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dog
+{
+    // instance variables - replace the example below with your own
+    String name;
+    String breed;
+    int age;
+    
+
+    /**
+     * Constructor for objects of class Dog
+     */
+    public Dog()
+    {
+        name = "scott";
+        System.out.println("Hello World");
+    }
+
+}
