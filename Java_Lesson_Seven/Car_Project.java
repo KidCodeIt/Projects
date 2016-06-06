7746b6776954dd61e5b2ac77953aeb3f351b51b7
Scott Campana <sbcampana@gmail.com>
2016-06-06 12:53:59 -0600

    Add files via upload

---
+++ Java_Lesson_Seven/Car_Project.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Car_Project here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car_Project
+{
+   public static void main(String [] args){
+       /*
+        * You should practice creating new classes, and creating methods for them
+        * create a Car class and create methods to set and get different values for the Car
+        * you should include model/ brand, year for the car, and color
+        * 
+        * as an extra challenge you can make them be read in using Scanner to assign information
+        * for each catagory listed above
+        */
+    }
+}
