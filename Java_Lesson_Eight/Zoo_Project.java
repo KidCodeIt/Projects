ab792e6d03d959e8b2e38bd677819b2bba604e72
Scott Campana <sbcampana@gmail.com>
2016-06-06 12:55:19 -0600

    Add files via upload

---
+++ Java_Lesson_Eight/Zoo_Project.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Zoo_Project here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zoo_Project
+{
+    public static void main(String [] args){
+        /*
+         * For this project you can use one of the created Zoo constructors to create animals
+         * you should read in using Scanner
+         * read in values for name, animal type, age, and gender of the animal
+         * you should practice storing them in either an array, arrayList, or hashMap
+         * 
+         */
+    }
+}
