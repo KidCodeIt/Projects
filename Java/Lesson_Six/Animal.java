305ab4a1210c91dba801b1f4697337c3a866b205
Scott Campana <sbcampana@gmail.com>
2016-06-08 13:23:42 -0600

    Add files via upload

---
+++ Java/Lesson_Six/Animal.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Animals here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Animal
+{
+   public Animal(){
+       System.out.println("You made an animal!");
+    }
+}
---
+++ Java/Lesson_Six/Dog.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Dog here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dog
+{
+    /**
+     * Constructor for objects of class Dog
+     */
+    public Dog()
+    {
+        System.out.println("You created a Dog!");
+    }
+
+}
